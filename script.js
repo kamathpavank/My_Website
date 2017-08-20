8fbb734ecd380eda746e0a50c047e10964e59c05
pavankamath1406 <pavan.g.kamath@gmail.com>
2017-08-20 18:55:05 +0530

    My Bootstrap websites

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+
+$(function () {
+
+  $('a.pop').click(function(e){
+    e.preventDefault();
+  });
+
+$('a.pop').popover();
+$('[rel="tooltip"]').tooltip();
+
+})
